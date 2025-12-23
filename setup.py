da88e046391b8b12a5b67ed7133b4b4b94963173
anoushkaa15 <161074890+anoushkaa15@users.noreply.github.com>
2025-12-23 14:57:01 +0530

    Add setup script for project dependencies
    Added installation commands for dependencies and cloned the repository.

---
+++ setup.py
@@ -0,0 +1,11 @@
+!git clone https://github.com/abuBakarSiddiqurRahman/DepressionEmo.git
+
+%cd DepressionEmo
+
+!pip install numpy>=1.21 pandas>=1.3 nltk>=3.6 scikit-learn>=1.0 matplotlib>=3.4 torch>=1.11 transformers>=4.20 xgboost>=1.6 lightgbm>=3.3
+
+pip install numpyencoder
+
+!pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+!pip install transformers==4.45.1  # or the latest available
+
